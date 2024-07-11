fee661edb591e5aa49f038c22ab1114447535526
rojsiroj <sirojudin.dev@gmail.com>
2024-07-11 21:53:20 +0700

    chore: add dotenv config

--- core/app.ts
+++ core/app.ts
@@ -1,11 +1,12 @@
 import express, { Application, Response } from "express";
+import dotenv from "dotenv";
 
 const app: Application = express();
+dotenv.config();
 
 app.get("/", (_, res: Response) => res.send("Hello World!"));
 
-const port = process.env.port || 8080;
-
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
